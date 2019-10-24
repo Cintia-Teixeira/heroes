29691a30ea68ed965c1864d0b52c5f2cf82090c2
Cintia <cin.contaadm@gmail.com>
2019-10-24 04:27:56 -0300

    Primeiros passos para exibir heróis individualmente.

--- src/heroes.controller.ts
+++ src/heroes.controller.ts
@@ -4,16 +4,17 @@ import { Controller, Get, Param } from '@nestjs/common';
 export class HeroesController {
    
     @Get()
-    getList(): string[] {
-        return [
-            "Batman",
-            "Superman"
-        ];     
+    getList(): string {
+        return 
+            'Batman'
+            'Superman'
+        ;     
      
     }
 
     @Get(':id')
-    findHeroe(@Param('id') id): string {
-        return `Heroe #${id}`;
+    findHeroe(@Param() params): string {
+        console.log(params.name);
+        return `Heroe #${params.id} is ${params.name}`;
     }
 }
