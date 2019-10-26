6bc14fd7e9b0501572f4c277222aa5f843b893a2
Cintia <cin.contaadm@gmail.com>
2019-10-26 00:01:09 -0300

    Modificando lista de heróis e os métodos da classe controller

--- src/heroes.controller.ts
+++ src/heroes.controller.ts
@@ -1,21 +1,31 @@
 import { Controller, Get, Param, Delete, Post } from '@nestjs/common';
 
 @Controller('heroes')
-export class HeroesController { 
-    
-    listHeroes = ["Batman", "Superman"];
-    
+export class HeroesController {
+
+    listHeroes: [
+        {
+            id: 1,
+            nome: 'Batman',
+            idade: 35
+        },
+        {
+            id: 2,
+            nome: 'Superman',
+            idade: 27
+        }
+
+    ];
 
     @Get()
-    getList(): string[] {
+    async getList(): Promise<any> {
         return this.listHeroes;
 
     }
 
-    @Get(':id')
-    findHeroe(@Param() params): string {
-
-        return this.listHeroes[params.id];
+    @Get('id')
+    findHeroe = this.listHeroes.find(function(element) {
+        return element.id;
     }
 
     @Delete(':id')
