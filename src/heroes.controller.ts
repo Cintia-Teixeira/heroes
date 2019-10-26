6b25351516e084aee42e0dbd51f8c505e9a7caac
Cintia <cin.contaadm@gmail.com>
2019-10-26 17:05:15 -0300

    Manipulando o array.

--- src/heroes.controller.ts
+++ src/heroes.controller.ts
@@ -3,44 +3,41 @@ import { Controller, Get, Param, Delete, Post } from '@nestjs/common';
 @Controller('heroes')
 export class HeroesController {
 
-    listHeroes: [
+    heroes = [
         {
             id: 1,
-            nome: 'Batman',
+            nome: "Batman",
             idade: 35
         },
+
         {
             id: 2,
-            nome: 'Superman',
+            nome: "Superman",
             idade: 27
         }
-
     ];
 
     @Get()
-    async getList(): Promise<any> {
-        return this.listHeroes;
-
+    list(): object {
+        return this.heroes;
     }
 
-    @Get('id')
-    findHeroe = this.listHeroes.find(function(element) {
-        return element.id;
+    @Post(':id/:nome/:idade')
+    create(@Param('id') id: number, @Param('nome') nome: string, @Param('idade') idade: number) {
+        return this.heroes.push({ id, nome, idade });
+    }
+
+    @Get(':id')
+    findHeroe(@Param() params: string) {
+        return this.heroes;
     }
 
     @Delete(':id')
     removeHeroe(@Param('id') id: number) {
+        for (var i = id; i < this.heroes.length; i++) {
 
-        return this.listHeroes.splice(id, 1);
-    }
+            this.heroes.splice((i), 1);
 
-    @Post(':name')
-    create(@Param('name') name: string) {
-        return this.listHeroes.push(name);
-    }
-
-    @Post(':id/:name')
-    update(@Param('id') id: number, @Param('name') name: string) {
-        return this.listHeroes.splice(id, 1, name);
+        }
     }
 }
