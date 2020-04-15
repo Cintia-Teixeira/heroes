10fe61c309de3918450be496694cbab5a4d85478
Cintia <cin.contaadm@gmail.com>
2020-04-14 21:09:35 -0300

    Começando a implementar a validação.

--- src/heroes/hero-dto.ts
+++ src/heroes/hero-dto.ts
@@ -1,6 +1,11 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class HeroDto {
 
     id: number;
+
+    @IsNotEmpty()
     name: string;
+    
     age: number;
 }
--- src/users/user-dto.ts
+++ src/users/user-dto.ts
@@ -1,9 +1,15 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class UserDto {
 
     name: string;
     age: number;
     email: string;
+
+    @IsNotEmpty()
     username: string;
+
+    @IsNotEmpty()
     password: string;
 
 }
